b102c6dc8ee095bc58088ea824616a8c1f9267e0
norbusonam <nsonam1822@gmail.com>
2022-08-17 22:05:17 -0700

    added messages

--- frontend/src/data/Rooms.js
+++ frontend/src/data/Rooms.js
@@ -4,15 +4,31 @@ export const Rooms = [
     _id: 'a',
     emoji: '🥾',
     name: 'Hikers of Washington',
+    messages: [
+      {
+        _id: 'm1',
+        user: 'a',
+        body: 'Hey!',
+      },
+      {
+        _id: 'm2',
+        user: 'b',
+        body: 'Hey, how are you?',
+      },
+    ],
   },
   {
     _id: 'b',
     emoji: '💗',
     name: 'Single Group',
+    messages: [
+    ],
   },
   {
     _id: 'c',
     emoji: '🧑‍🎓',
     name: 'RPI Alumn',
+    messages: [
+    ],
   }
 ];
