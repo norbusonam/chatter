2709a918ccac646d77ab69d074b0dc6178bb3863
norbusonam <nsonam1822@gmail.com>
2022-08-18 22:16:40 -0700

    updated data to closer resemble api requests

--- frontend/src/data/Rooms.js
+++ frontend/src/data/Rooms.js
@@ -1,34 +1,17 @@
-// TODO: room's shouldnt be hard coded
 export const Rooms = [
   {
     _id: 'a',
     emoji: '🥾',
     name: 'Hikers of Washington',
-    messages: [
-      {
-        _id: 'm1',
-        user: 'a',
-        body: 'Hey!',
-      },
-      {
-        _id: 'm2',
-        user: 'b',
-        body: 'Hey, how are you?',
-      },
-    ],
   },
   {
     _id: 'b',
     emoji: '💗',
     name: 'Single Group',
-    messages: [
-    ],
   },
   {
     _id: 'c',
     emoji: '🧑‍🎓',
     name: 'RPI Alumn',
-    messages: [
-    ],
   }
 ];
