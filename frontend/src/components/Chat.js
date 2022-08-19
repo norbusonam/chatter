e95b9a68bbb9992a9fb9873bf33a0598fd450f5b
norbusonam <nsonam1822@gmail.com>
2022-08-18 22:17:55 -0700

    send and receive new messages

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,14 +1,34 @@
+import { useEffect, useState } from "react";
 import ChatHeader from "./ChatHeader";
 import ChatMessages from "./ChatMessages";
 import ChatNewMessage from "./ChatNewMessage";
+import io from 'socket.io-client';
+import { Messages } from '../data/Messages';
+
+const socket = io("localhost:3001");
 
 export default function Chat({ room }) {
+
+  const [messages, setMessages] = useState(Messages);
+
+  useEffect(() => {
+
+    socket.on('new:message', (msg) => {
+      setMessages([...messages, msg]);
+    })
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+    };
+    
+  }, [messages]);
 
   return (
     <div className='grow bg-gray-700 flex flex-col'>
       <ChatHeader roomEmoji={room.emoji} roomName={room.name} />
-      <ChatMessages messages={room.messages}/>
-      <ChatNewMessage />
+      <ChatMessages messages={messages}/>
+      <ChatNewMessage socket={socket} />
     </div>
   )
 
--- frontend/src/components/ChatNewMessage.js
+++ frontend/src/components/ChatNewMessage.js
@@ -1,28 +1,29 @@
 import { useState } from "react";
 
-export default function ChatNewMessage() {
+export default function ChatNewMessage({ socket }) {
 
-  const [messageHeight, setMessageHeight] = useState(10);
+  const [message, setMessage] = useState('');
 
-  const sendMessage = () => {
-    // TODO: send message logic
-  };
-
-  const updateHeight = (event) => {
-    // TODO: this logic still needs work
-    setMessageHeight(10 + parseInt((event.target.scrollHeight - 40) / 20) * 10)
+  const sendMessageIfEnter = (e) => {
+    if (e.code === 'Enter') {
+      socket.emit('create:message', {
+        user: 'user._id',
+        body: message,
+      });
+      setMessage('');
+    }
   };
   
   return (
     <div className="pb-2 px-4">
-      <form onSubmit={sendMessage}>
-        <textarea
-          onKeyUp={updateHeight}
-          className={`w-full h-${Math.max(messageHeight, 10)} px-3 pt-2 bg-gray-500 text-gray-100 rounded-md resize-none`}
-          placeholder="Send message...">
-        </textarea>
-      </form>
+      <textarea
+        onKeyDown={sendMessageIfEnter}
+        onChange={e => setMessage(e.target.value)}
+        value={message}
+        className={`w-full h-10 px-3 pt-2 bg-gray-500 text-gray-100 rounded-md resize-none`}
+        placeholder="Send message...">
+      </textarea>
     </div>
-  )
+  );
 
 }
