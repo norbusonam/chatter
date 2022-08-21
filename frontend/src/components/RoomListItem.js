b2a70e424e6691b0da7568ff0fa912a37fb16e34
norbusonam <nsonam1822@gmail.com>
2022-08-20 23:00:03 -0700

    mistyped class

--- frontend/src/components/RoomListItem.js
+++ frontend/src/components/RoomListItem.js
@@ -2,7 +2,7 @@ export default function RoomListItem({ room, isSelected, setRoom }) {
 
   return (
     <button 
-      className={`w-11/12 text-gray-100 hover:bg-gray-700 hover:text-opacity-80 transition-colors trans rounded-md p-2 text-left mb-1 ${isSelected ? 'text-opacity-100 bg-gray-700' : 'text-opacity-60'}`}
+      className={`w-11/12 text-gray-100 hover:bg-gray-700 hover:text-opacity-80 transition-colors rounded-md p-2 text-left mb-1 ${isSelected ? 'text-opacity-100 bg-gray-700' : 'text-opacity-60'}`}
       onClick={() => setRoom(room)}
     >
       <p><span>{room.emoji}</span> {room.name}</p>
