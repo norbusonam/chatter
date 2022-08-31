a6688988b701d5657f2223a20d71803a1d21214c
norbusonam <nsonam1822@gmail.com>
2022-08-30 22:35:47 -0700

    globally set '_id' -> 'id', no more '__v'

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,3 +5,12 @@ module.exports = () => {
     .then(() => console.log("Successfully connected to database"))
     .catch(() => console.log("Failed to connected to database"));
 }
+
+mongoose.set('toJSON', {
+  virtuals: true,
+  transform: (doc, converted) => {
+    delete converted._id;
+    delete converted.__v;
+  },
+  
+});
