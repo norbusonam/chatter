a667f3a71b5c335352618b1b3f8d7eae01bd961d
norbusonam <nsonam1822@gmail.com>
2022-09-03 18:44:06 -0700

    updated to search for only user's rooms

--- backend/controller/room/query.js
+++ backend/controller/room/query.js
@@ -1,5 +1,6 @@
 const { query } = require('express-validator');
 const Room = require('../../models/Room');
+const UserInRoom = require('../../models/UserInRoom');
 
 
 module.exports = {
@@ -7,18 +8,37 @@ module.exports = {
   validations: [
     query('query')
       .isString(),
+    query('onlyMine')
+      .isBoolean()
+      .isIn(['true', 'false']),
   ],
   
   fn: async (req, res) => {
 
-    const { query } = req.query
+    const { query, onlyMine } = req.query
 
-    const rooms = await Room.find({
+    const filter = {
       $or: [
         { name: { $regex: '.*' + query + '.*' } },
         { description: { $regex: '.*' + query + '.*' } },
-      ]
-    });
+      ],
+    }
+
+    // TODO: find a better way to parse query booleans
+    if (onlyMine.toLowerCase() === 'true') {
+      const userRooms = await UserInRoom.find(
+        { user: req.userId },
+        ['room']
+      );
+      console.log(userRooms.map(userRoom => userRoom.room));
+      filter._id = {
+        $in: userRooms.map(userRoom => userRoom.room),
+      };
+    }
+
+    console.log(filter);
+
+    const rooms = await Room.find(filter);
 
     return res.send(rooms);
 
