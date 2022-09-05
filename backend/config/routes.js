d16925e0104f1a534117ceaaa89c8832cebc6e5c
norbusonam <nsonam1822@gmail.com>
2022-09-04 18:36:09 -0700

    user in room routing and controller set up

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -4,23 +4,32 @@ const getUser = require('../controller/user/get');
 const getMe = require('../controller/user/me');
 const createRoom = require('../controller/room/create');
 const queryRooms = require('../controller/room/query');
+const joinRoom = require('../controller/user-in-room/create');
+const queryUsersInRoom = require('../controller/user-in-room/query');
+const leaveRoom = require('../controller/user-in-room/delete');
 const queryMessages = require('../controller/message/query');
 const checkAuth = require('../middleware/http/checkAuth');
 const validate = require('../middleware/http/validate');
+const userIsInRoom = require('../middleware/http/userIsInRoom');
 
 module.exports = (app) => {
 
-  // room routes
-  app.post('/room', checkAuth, validate(createRoom.validations), createRoom.fn);
-  app.get('/room', checkAuth, validate(queryRooms.validations), queryRooms.fn);
-
   // user routes
-  app.post('/user/login', validate(loginUser.validations), loginUser.fn);
-  app.post('/user/signup', validate(createUser.validations), createUser.fn);
-  app.get('/user/me', checkAuth, getMe.fn);
-  app.get('/user/:username', validate(getUser.validations), getUser.fn);
+  app.post   ('/user/login', validate(loginUser.validations), loginUser.fn);
+  app.post   ('/user/signup', validate(createUser.validations), createUser.fn);
+  app.get    ('/user/me', checkAuth, getMe.fn);
+  app.get    ('/user/:username', validate(getUser.validations), getUser.fn);
+
+  // room routes
+  app.post   ('/room', checkAuth, validate(createRoom.validations), createRoom.fn);
+  app.get    ('/room', checkAuth, validate(queryRooms.validations), queryRooms.fn);
+  
+  // user in room routes
+  app.post   ('/room/:roomId/user', checkAuth, validate(joinRoom.validations), joinRoom.fn);
+  app.delete ('/room/:roomId/user', checkAuth, userIsInRoom, validate(leaveRoom.validations), leaveRoom.fn);
+  app.get    ('/room/:roomId/user', checkAuth, userIsInRoom, validate(queryUsersInRoom.validations), queryUsersInRoom.fn);
 
   // message routes
-  app.get('/room/:roomId/message', validate(queryMessages.validations), queryMessages.fn);
+  app.get    ('/room/:roomId/message', checkAuth, userIsInRoom, validate(queryMessages.validations), queryMessages.fn);
 
 }
---
+++ backend/controller/user-in-room/query.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  validations: [
+
+  ],
+
+  fn: (req, res) => {
+
+  }
+  
+}
