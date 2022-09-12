6912ff0b1a5fed1cfab3f0d2d7b7efd1ab4cfdea
norbusonam <nsonam1822@gmail.com>
2022-09-11 22:01:47 -0700

    api progress

--- frontend/src/helpers/api.js
+++ frontend/src/helpers/api.js
@@ -3,16 +3,45 @@ import axios from "axios";
 const API_URL = process.env.REACT_APP_API_URL
 
 export const getMe = () => {
-  console.log(API_URL);
-  return axios.get()
+  const authToken = window.localStorage.getItem('authToken');
+  return axios.get(API_URL + 'user/me', {
+    auth: authToken,
+  });
+}
+
+export const login = (payload) => {
+  return axios.post(API_URL + 'user/login', payload);
+}
+
+export const signup = (payload) => {
+  return axios.post(API_URL + 'user/signup', payload);
+}
+
+export const createRoom = (payload) => {
+  const authToken = window.localStorage.getItem('authToken');
+  return axios.post(API_URL + 'room', payload, {
+    auth: authToken,
+  });
 }
 
 export const getRooms = () => {
-  console.log(API_URL);
-  return axios.get()
+  // just gets user's rooms for now
+  const authToken = window.localStorage.getItem('authToken');
+  return axios.get(API_URL + 'room?onlyMine=true', {
+    auth: authToken,
+  });
 }
 
-export const getMessages = () => {
-  console.log(API_URL);
-  return axios.get()
+export const getMessages = (roomId, payload) => {
+  const authToken = window.localStorage.getItem('authToken');
+  return axios.get(API_URL + `room/${roomId}/message`, {
+    auth: authToken,
+  });
+}
+
+export const getUsersInRoom = (roomId, payload) => {
+  const authToken = window.localStorage.getItem('authToken');
+  return axios.get(API_URL + `room/${roomId}/user`, {
+    auth: authToken,
+  });
 }
