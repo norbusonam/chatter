7a1404ea9b1047b68466387f34396e679931763c
norbusonam <nsonam1822@gmail.com>
2022-09-13 20:54:30 -0700

    set up cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const registerRoutes = require('./config/routes');
 const setupSockets = require('./config/sockets');
@@ -12,6 +13,10 @@ dotenv.config();
 // server and socket setup
 const app = express();
 app.use(express.json());
+app.use(cors({
+  // TODO: make this an enviornment variable
+  origin: ['http://localhost:3000']
+}));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
