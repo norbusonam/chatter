d2ad49ada006f233faaece3733e8f5e0dfb094e5
norbusonam <nsonam1822@gmail.com>
2022-09-19 22:09:48 -0700

    barebones explore rooms

--- frontend/src/components/ExploreRooms.js
+++ frontend/src/components/ExploreRooms.js
@@ -1,11 +1,52 @@
-export default function ExploreRooms({ roomEmoji, roomName }) {
+import { useEffect, useState } from "react"
+import { getRooms } from "../helpers/api";
+
+export default function ExploreRooms({ setRooms }) {
+
+  const [query, setQuery] = useState('');
+  const [roomsFromQuery, setRoomsFromQuery] = useState([]);
+  const [isRoomsFromQueryLoading, setIsRoomsFromQueryLoading] = useState(false);
+
+  useEffect(() => {
+    queryRooms();
+  }, [])
+
+  const queryRooms = () => {
+    setIsRoomsFromQueryLoading(true);
+    getRooms({ onlyMine: false, query })
+      .then((res) => {
+        console.log(res.data)
+        setRoomsFromQuery(res.data.rooms);
+        setQuery('');
+      })
+      .catch(err => {
+        console.log(err);
+      }) 
+      .finally(() => setIsRoomsFromQueryLoading(false));
+  }
+  
+  const onSearch = (e) => {
+    e.preventDefault();
+    queryRooms();
+  }
 
   return (
     <div className='grow bg-gray-700 flex flex-col max-h-screen'>
       <h1>Explore rooms!</h1>
-      <form>
-        <input type='text'></input>
+      <form onSubmit={onSearch}>
+        <input type='text' onChange={e => setQuery(e.target.value)}></input>
       </form>
+      {
+        isRoomsFromQueryLoading ?
+          <p>loading...</p>
+        :
+          roomsFromQuery.map(room => (
+            <div key={room.id}>
+              <h1>{room.emoji} {room.name}</h1>
+              <p>{room.description}</p>
+            </div>
+          ))
+      }
     </div>
   )
 
