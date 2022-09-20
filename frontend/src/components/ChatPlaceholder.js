39c68a964af4c344085609670529c63c7663891b
norbusonam <nsonam1822@gmail.com>
2022-09-19 21:30:13 -0700

    replace placeholder with explore rooms

--- frontend/src/components/ChatPlaceholder.js
+++
@@ -1,9 +0,0 @@
-export default function ChatPlaceholder() {
-
-  return (
-    <div className="grow bg-gray-700 flex items-center justify-center text-gray-100">
-      Select a room to get started
-    </div>
-  )
-
-}
---
+++ frontend/src/components/ExploreRooms.js
@@ -0,0 +1,12 @@
+export default function ExploreRooms({ roomEmoji, roomName }) {
+
+  return (
+    <div className='grow bg-gray-700 flex flex-col max-h-screen'>
+      <h1>Explore rooms!</h1>
+      <form>
+        <input type='text'></input>
+      </form>
+    </div>
+  )
+
+}
