1c255fe4400cf527d5234e473e55f458c6b03576
norbusonam <nsonam1822@gmail.com>
2022-09-21 20:45:13 -0700

    barebones message sender in ux

--- frontend/src/components/ChatMessage.js
+++ frontend/src/components/ChatMessage.js
@@ -3,7 +3,7 @@ export default function ChatMessage({ message }) {
   return (
     <div>
       <div className="px-4 py-2 my-1 hover:bg-gray-600 text-gray-100">
-        { message.body }
+        <span className="font-bold text-gray-400">{ message.from.username }:</span> { message.body }
       </div>
     </div>
   )
