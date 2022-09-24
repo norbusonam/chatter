07ccb02d710c95244fc00e51b134a40665e03ec5
norbusonam <nsonam1822@gmail.com>
2022-09-24 14:23:25 -0700

    ensure sidebar does not shrink

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -5,7 +5,7 @@ import UserCard from "./UserCard";
 export default function Sidebar({ currentRoom, setCurrentRoom, rooms, user, logoutUser }) {
 
   return (
-    <div className="w-60 h-screen flex flex-col">
+    <div className="w-60 flex-shrink-0 h-screen flex flex-col">
       <RoomList currentRoom={currentRoom} setCurrentRoom={setCurrentRoom} rooms={rooms}/>
       <UserCard user={user} logoutUser={logoutUser}/>
     </div>
